a330cc82ebdb1feeabf9a3113908e6f2e1b9a0d8
RJ648 <ribeiroleandro648@gmail.com>
2024-07-01 09:05:37 +0100

    Add files via upload

---
+++ js/cart.js
@@ -0,0 +1,1 @@
+const payBtn=document.querySelector(".btn-buy");payBtn.addEventListener("click",()=>{fetch("/stripe-checkout",{method:"post",headers:new Headers({"Content-Type":"application/Json"}),body:JSON.stringify({items:JSON.parse(localStorage.getItem("cartItems"))})}).then(a=>a.json()).then(a=>{location.href=a,clearCart()}).catch(a=>console.log(a))});
